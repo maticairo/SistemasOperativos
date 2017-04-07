d81c8f2b2c1e5ab8e945a38ba0e7aac1ab685673
Matias Cairo <maticairo@gmail.com>
2017-04-06 21:48:45 -0300

    Ejercicio 2 TP1

--- Ej2TP1.ps1
+++ Ej2TP1.ps1
@@ -1,14 +1,21 @@
 ﻿#LEER BIEN EL ENUNCIADO PARA VER QUE FALTA!
 #Hay que validar parametros y poner la ayuda para Get-Help - ver enunciado
-Param($pathLectura, $pathSalida, $pathLog)
+
+Param([Parameter(Mandatory=$true)]
+      [string] $pathLectura, 
+      [Parameter(Mandatory=$true)]
+      [string]$pathSalida, 
+      $pathLog)
 #Leo las Urls
 $lineas=Get-Content $pathLectura;
 #Instancio el WebClient
 $obj=New-Object System.Net.WebClient;
 foreach($linea in $lineas)
 {
-    #Obtengo el nombre del archivo a descargar - optimizar esto
-    $arch=$lineas.TrimStart('http://sisop.com.ar/files/catedra/apuntes/ejercicios/')
+    #Obtengo el nombre del archivo a descargar
+    $ultimaBarra=$linea.LastIndexOf('/')
+    $ultimaBarra++
+    $arch=$linea.substring($ultimaBarra)
     #Concateno el directorio de salida con el nombre de archivo correspondiente
     $pathSalidaFinal="$($pathSalida)$($arch)"
     #tomo la hora de inicio
@@ -24,7 +31,15 @@ foreach($linea in $lineas)
     #Armo la linea a imprimir en el log
     $imprimir="$($horaInicio) | $($horaFin) | $($tiempoTotal) | $($archivo.Length) Bytes"
     #imprimo el log efectivamente
-    $imprimir | Out-File 'salida.log'
+    if($pathLog -eq $null)
+    {
+        $imprimir | Out-File "$($pathSalida)salida.log" -Append
+    }
+    else{
+        
+        $imprimir | Out-File "$($pathLog)salida.log" -Append
+    }
+    
 }
 
 #FIN
