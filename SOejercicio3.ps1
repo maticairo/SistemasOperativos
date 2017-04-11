8c2c53aa8c83d9f1e5a42d6d669ed13d582dbde1
PabloAvalo <pabloavalo@outlook.com>
2017-04-12 01:58:19 +0200

    Add files via upload
    Le faltan unos detalles.
    No se si la salida es como lo pide, falta el format table

---
+++ SOejercicio3.ps1
@@ -0,0 +1,11 @@
+﻿$dict = @{};
+gci c:\prueba -Recurse  |where {$_.mode -notlike "d*"}| foreach {
+  $key = $_.Name 
+  $find = $dict[$key]
+  if($find -ne $null) {
+    #el actual es duplicado 
+        write $_.Name  $_.CreationTime  $_.LastWriteTime $_.DirectoryName
+  }
+  $dict[$key] ++;
+}
+
