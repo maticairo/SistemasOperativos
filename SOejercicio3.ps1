4d76f0a14d1e083fb0cd94ac1fea099f852d0e93
PabloAvalo <pabloavalo@outlook.com>
2017-04-11 21:35:01 -0300

    Update SOejercicio3.ps1

--- SOejercicio3.ps1
+++ SOejercicio3.ps1
@@ -5,12 +5,13 @@ gci c:\prueba -Recurse  |where {$_.mode -notlike "d*"}| foreach {
   if($find -ne $null) {
     #el actual es duplicado 
        # write $_.Name  $_.CreationTime  $_.LastWriteTime $_.DirectoryName
-       $salida += $_ | Format-Table -Property Name,CreationTime,LastWriteTime,DirectoryName 
+       $salida = $_ | Format-Table -Property Name,CreationTime,LastWriteTime,DirectoryName 
+       $sal += $salida
   }
   $dict[$key] ++;
 }
 
-$salida | Out-File -FilePath C:\prueba\prue.log
+$sal | Out-File -FilePath C:\prueba\prue.log
 
 
 
