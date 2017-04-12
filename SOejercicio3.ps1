2848191b0a516a5b36b8c0f5a4768d2d82bb19af
PabloAvalo <pabloavalo@outlook.com>
2017-04-11 21:15:48 -0300

    le agrego la salida y el format table
    falta ponerle los parámetros

--- SOejercicio3.ps1
+++ SOejercicio3.ps1
@@ -1,11 +1,16 @@
-﻿$dict = @{};
+$dict = @{};
 gci c:\prueba -Recurse  |where {$_.mode -notlike "d*"}| foreach {
   $key = $_.Name 
   $find = $dict[$key]
   if($find -ne $null) {
     #el actual es duplicado 
-        write $_.Name  $_.CreationTime  $_.LastWriteTime $_.DirectoryName
+       # write $_.Name  $_.CreationTime  $_.LastWriteTime $_.DirectoryName
+       $salida += $_ | Format-Table -Property Name,CreationTime,LastWriteTime,DirectoryName 
   }
   $dict[$key] ++;
 }
+
+$salida | Out-File -FilePath C:\prueba\prue.log
+
+
 
