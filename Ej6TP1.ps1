1a15671c4f7cc40b9de9002edc605d08f87c05fc
Matias Cairo <maticairo@gmail.com>
2017-04-15 12:07:55 -0300

    Ejercicio 6 TP1

---
+++ Ej6TP1.ps1
@@ -0,0 +1,35 @@
+﻿#LEER BIEN EL ENUNCIADO PARA VER QUE FALTA!
+#Hay que validar parametros y poner la ayuda para Get-Help - ver enunciado
+
+Param([Parameter(Mandatory=$true)]
+      [string] $pathLectura,  
+      $pathLog)
+
+#Función que alerta si se inició un proceso no deseado y lo registra en el log con el path correspondiente
+function AlertAndLog{
+    
+    Write-Output "Se inicio un proceso"
+    if($pathLog -eq $null){
+        $input | Out-File "$($pathLectura)salida.log" -Append
+    }
+    else{
+        $input | Out-File "$($pathLog)salida.log" -Append
+    }  
+}
+
+$procesos=Get-Content $pathLectura;
+if($procesos.Length -eq 0)
+{
+    #Archivo vacío
+    Write-Output "El archivo está vacio" 
+}
+else{
+    #Por cada proceso me suscribo al evento
+    foreach($proceso in $procesos)
+    {
+    Register -ObjectEvent -Event Started -InputObject $proceso -Action{
+        Get-Process $proceso | AlertAndLog
+        }
+    }
+}
+#FIN
