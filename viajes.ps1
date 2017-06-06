6886f3b63f4aa6510994fb050fe0461d0a92dce4
PabloAvalo <pabloavalo@outlook.com>
2017-06-06 19:06:55 -0300

    Viajes Final

--- viajes.ps1
+++ viajes.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 Trabajo Práctico 1
 Ejercicio 5
 Matias Ezequiel Cairo 39670522
@@ -6,28 +6,43 @@ Thomas Ignacio Reynoso 39332450
 Pablo Avalo 39214569
 Luciano Gabriel Tonlorenzi Sebastía 39244171
 Micaela Rocío De Rito 39547209 
+#>
 
+<#
 .SYNOPSIS
     
     Muestra un listado de pasajes disponibles
     
 .DESCRIPTION
    
-    Este script nos permite realizar una búsqueda de pasajes (a partir de un archivo csv) según el lugar de origen y destino que le ingresaremos por parámetros.
+    Este script nos permite realizar una búsqueda de pasajes (a partir de un archivo csv que se recibe por parametro) según el lugar de origen y destino que le ingresaremos por parámetros.
     Según lo que el usuario elija, se actualizará el archivo csv.
-
 #>
  
-Param($ciudadOrigen,$ciudadDestino,$cambio,$directorio)
+Param([Parameter(Mandatory=$true)]
+       [string]$csv,
+       [Parameter(Mandatory=$true)]
+       [string]$ciudadOrigen,
+       [Parameter(Mandatory=$true)]
+       [string]$ciudadDestino,
+       [int]$cambio)
 
 
-
-$bdd = Import-Csv -Path  $directorio
+$testcsv = Test-Path $csv
+if ( $testcsv -ne $false)
+{
+$bdd = Import-Csv -Path  $csv
+}
+else 
+{
+ echo " no se recibio csv valido "
+ return   
+}
 if ($bdd -ne $null){
     $filtro =$bdd | where{(($_.origen).ToUpper()).StartsWith($ciudadOrigen.ToUpper()) -and (($_.destino).ToUpper()).StartsWith($ciudadDestino.ToUpper()) -and $_.disponible -gt 0 -and (get-date $_.'Fecha Hora Desde') -gt (get-date)}
 
     if ($filtro -ne $null){
-        if ( $cambio -ne $null){
+        if ( $cambio -ne $null -and $cambio -gt 0 ){
             foreach ( $item in $filtro ) {
                 
                 $b = $item.precio/$cambio
@@ -49,7 +64,7 @@ if ($bdd -ne $null){
         }
         while(!$viaje -or ($viaje.disponible - $CantDePasajes) -lt 0 )
         $viaje.Disponible -= $CantDePasajes
-        $bdd|Export-Csv -path $directorio
+        $bdd|Export-Csv -path $csv
     }
     else {
         write-host  "No encontramos viajes para el destino especificado" 
