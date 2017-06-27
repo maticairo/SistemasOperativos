c15a88aaab8509a2ae33a7059acdb3aacf78c060
Thomas <thomi.reynoso@hotmail.com>
2017-06-26 23:38:02 -0300

    Se arregla el ej3tp2

--- BASH/ej3tp2.sh
+++ BASH/ej3tp2.sh
@@ -5,17 +5,17 @@
 #Pablo Avalo 39214569
 #Luciano Gabriel Tonlorenzi Sebastía 39244171
 #De Rito Micaela 39547209
-#Primera Entrega
+#Segunda Entrega
 
 #!/bin/bash
 
 ### Ayuda
 
+script_name=$(basename "$0")
 
 function ayuda {
 printf "
 \e[1mUso\e[0m
-
          	./ej3tp2.sh [fecha_mínima] [fecha_máxima] [nom_comprimido] [directorio_destino] [directorio_origen]
  
 \e[1mParametros\e[0m          
@@ -23,28 +23,39 @@ printf "
 		1 un string con el nombre del directorio donde se comprimiran el/los archivos,
 		1 directorio donde se alojaran el/los archivos archivos_a_comprimir
 		[Opcional] 1 directorio donde se buscaran los archivos a comprimir
-
 \e[1mDescripción\e[0m
                 Lee los archivos que cumplen con un formato especificado (En este caso son .txt) que se encuentren en un rango de fechas, luego mueve dichos archivos a un directorio, el cual luego es comprimido y movido al [directorio_destino] pasado por parametro. Tambien se crea un archivo.log con los archivos archivos_a_comprimir
                
 \e[1mEjemplo\e[0m
        
-               bash ./ej3tp2.sh 10/05/2017 12/05/2017 compri /Escritorio
-
-\e[1mNOTA\e[0m
-	
-		La fecha del find esta hardcodeada ya que no encuentro la manera de que el newermt me lea la fecha pasada por parametro, todas las soluciones que encontre, no pude implementarlas! \n\n"
-
+               bash ./ej3tp2.sh 10-05-2017 12-05-2017 compri /home/su_usuario/Escritorio
+\e[1mNOTA\e[0m"
+		"su_usuario es el nombre que posee su maquina" 
 }
 
 
+function dateIsValid(){
+	if [[ $1 =~ ^[0-9]{4}[0-9]{2}[0-9]{2}$ ]] && date -d "$1" >/dev/null; then
+	      return 0
+	fi
+	return 1
+}
+
+
+
+# Validaciones de parametros
+
 if [ "$1" = "-h" -o "$1" = "-help" -o "$1" = "-?" ] ; then ayuda ; exit ; fi
+
+if [[ $# -lt 4 ]] || [[ $# -gt 5 ]]; then
+	echo "Cantidad ingresada de parametros errónea. Por favor ingrese entre 4 y 5. Ejecute el comando -h para mas ayuda!"
+fi
 
 ### pregunto si algunos de los argumentos estan vacios y muestro el mensaje correspondiente
 
 if [ -z $1 ];then
   echo "No se ingreso la fecha minima."
-  exit 0
+  exit 0	
 fi
 
 if [ -z $2 ];then
@@ -62,6 +73,7 @@ if [ -z $4 ];then
   exit 0
 fi
 
+
 #Parametro opcional 
 
 if [ -z $5 ];then
@@ -70,28 +82,55 @@ else
   directorio_origen=$5
 fi
 
-#***Intentos de transformar la fecha para que el find las pueda leer ***
+# Convierto las fechas a formato YYYYMMDD
 
-#fecha_minima=$(echo $1 | awk '{split($1,a,"-");$1=a[3]"/"a[2]"/"a[1]}1')
-#fecha_maxima=$(echo $2 | awk '{split($2,a,"-");$2=a[3]"/"a[2]"/"a[1]}1')
+fecha_desde=`echo $1 | awk -F - '{print $3$2$1}'`
+fecha_hasta=`echo $2 | awk -F - '{print $3$2$1}'`
 
-#fecha_minima= $(date -d "$1" +"%F")
-#fecha_maxima= $(date -d "$2" +"%F")
+#Valido que las fechas ingresadas sean correctas
+
+if ! dateIsValid $fecha_desde; then
+	echo "La fecha mínima es incorrecta"
+	exit 0
+fi
+	
+if ! dateIsValid $fecha_hasta; then
+	echo "La fecha máxima es incorrecta"
+	exit 0
+fi
+
+if [[ $fecha_desde > $fecha_hasta ]]; then
+	echo "La fecha mínima debe ser menor a la fecha máxima"
+	exit 0
+fi
 
 
 nombre_comprimido=$3
 directorio_destino=$4
 
-#se crea el directorio donde se moveran los archivos a comprimir 
-mkdir a_comprimir
-touch $nombre_comprimido.log
-for i in ` find $directorio_origen/*.txt -type f -newermt "2017-05-10" ! -newermt "2017-05-20" `; do
-  mv $i ./a_comprimir
-  nombre=$(basename "$i")
-  echo  $nombre >> $nombre_comprimido.log
-done
+log_file="$directorio_destino/$nombre_comprimido.log"
+zip_file="$directorio_destino/$nombre_comprimido.tar.gz"
 
-tar -czf $nombre_comprimido.tar.gz ./a_comprimir
-rm -r a_comprimir
-mv $nombre_comprimido.tar.gz $directorio_destino
-mv $nombre_comprimido.log $directorio_destino
+#Busco los archivos en el rango de fechas dado
+
+find $directorio_origen -type f -newermt $fecha_desde ! -newermt $fecha_hasta -printf "%P\n" > $log_file
+
+
+#Verifico si el directorio donde se encuentran los archivos a comprimir, esta vacio o no
+
+if [ "$(ls -A $directorio_origen)" ]; then
+	
+	# Comprimo y elimino usando el archivo log generado anteriormente
+	
+	if [[ -s "$log_file" ]]; then
+		tar czf "$zip_file" -C "$directorio_origen" -T "$log_file" --exclude $script_name --remove-files
+	else
+		rm "$log_file"
+	fi
+else 
+
+	echo "El directorio a comprimir esta vacio"
+
+fi
+
+exit 0
